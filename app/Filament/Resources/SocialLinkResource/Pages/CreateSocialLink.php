51ee76020827f2faeb82390f42e930c49de70269
Sergii <dragnil589@gmail.com>
2023-04-19 02:10:45 +0300

    add seed and add form to admin social link

---
+++ app/Filament/Resources/SocialLinkResource/Pages/CreateSocialLink.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SocialLinkResource\Pages;
+
+use App\Filament\Resources\SocialLinkResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSocialLink extends CreateRecord
+{
+    protected static string $resource = SocialLinkResource::class;
+}
