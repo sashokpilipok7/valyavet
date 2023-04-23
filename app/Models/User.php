a4fda3056d16028bb3b887671e024f6744c366b7
Sasha Pylypenko <Sashapylypenko.pov@gmail.com>
2023-04-23 22:26:21 +0300

    fix: admin access

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@xxx.com');
     }
 }
